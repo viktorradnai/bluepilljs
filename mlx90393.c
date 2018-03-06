7310d7329f69bb4e8df06b6b25edd501774ba377
Viktor Radnai <viktor.radnai@gmail.com>
2018-03-06 02:10:58 +0000

    Explain MLX90393 commands

--- mlx90393.c
+++ mlx90393.c
@@ -9,22 +9,27 @@ extern SerialUSBDriver SDU1;
 
 bool_t mlx90393_init(void)
 {
-    uint8_t out1[] = { 0x60, 0x00, 0x5C, 0x00 << 2 };
-    uint8_t out2[] = { 0x60, 0x02, 0xB4, 0x02 << 2 };
-    uint8_t out3[] = { 0x50, 0x02 << 2 };
+    // MLX903939 Write register format: CMD, DATA_H, DATA_L, ADDR << 2
+    // Address must be shifted up by 2 bits
+
+    uint8_t out1[] = { MLX90393_CMD_WRITE_REG, 0x00, 0x5C, 0x00 << 2 }; // GAIN_SEL = 0x5, HALLCONF = 0xC
+    uint8_t out2[] = { MLX90393_CMD_WRITE_REG, 0x04, 0x00, 0x01 << 2 }; // TCMP_EN = 1
+    uint8_t out3[] = { MLX90393_CMD_WRITE_REG, 0x02, 0xB4, 0x02 << 2 }; // RES_XYZ = 0b010101, DIG_FILT = 0b100
+    uint8_t out4[] = { MLX90393_CMD_READ_REG, 0x02 << 2 };
     uint8_t in[4];
 
     if (!i2c_write(MLX90393_I2C_ADDR, out1, sizeof(out1), in, 2)) return FALSE;
     if (!i2c_write(MLX90393_I2C_ADDR, out2, sizeof(out2), in, 2)) return FALSE;
-    return i2c_write(MLX90393_I2C_ADDR, out3, sizeof(out3), in, 4);
+    if (!i2c_write(MLX90393_I2C_ADDR, out3, sizeof(out3), in, 2)) return FALSE;
+    return i2c_write(MLX90393_I2C_ADDR, out4, sizeof(out4), in, 4);
 }
 
 bool_t mlx90393_read(int16_t* data)
 {
     uint8_t in[8];
 
-    uint8_t out1[] = { 0x3E };
-    uint8_t out2[] = { 0x4E };
+    uint8_t out1[] = { MLX90393_CMD_MEASURE_XYZ };
+    uint8_t out2[] = { MLX90393_CMD_READ_XYZ };
 
     if (!i2c_write(MLX90393_I2C_ADDR, out1, sizeof(out1), in, 2)) return FALSE;
 
