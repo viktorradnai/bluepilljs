443c4b35d7f4e26ce36dc9066c6fd34f6d620950
Viktor Radnai <viktor.radnai@gmail.com>
2018-03-19 23:17:44 +0000

    Change 1 element int array to int

--- lsm303.c
+++ lsm303.c
@@ -10,15 +10,15 @@ extern SerialUSBDriver SDU1;
 
 bool_t lsm303c_init(void)
 {
-    uint8_t ident_out[] = { 0x0F }; // Highest speed
-    uint8_t ident_in[1];
+    uint8_t ident_out = 0x0F; // Highest speed
+    uint8_t ident_in;
     uint8_t out1[] = { 0x20, 0x7C }; // Highest speed
     uint8_t out2[] = { 0x21, 0x60 }; // Lowest gain
     uint8_t out3[] = { 0x22, 0x00 }; // Continuous conversion mode
     uint8_t out4[] = { 0x23, 0x0E };
 
-    if (!i2c_write(LSM303C_I2C_ADDR, ident_out, sizeof(ident_out), ident_in, sizeof(ident_in))) return FALSE;
-    if (ident_in[0] != 0x3D) {
+    if (!i2c_write(LSM303C_I2C_ADDR, &ident_out, sizeof(ident_out), &ident_in, sizeof(ident_in))) return FALSE;
+    if (ident_in != 0x3D) {
         printf("Device at address LSM303C_I2C_ADDR does not appear to be an LSM303C\r\n");
         return FALSE;
     }
